e075210d8a20241513b7f8148bc460c3c88041d2
OnurYilmazGit <46044317+OnurYilmazGit@users.noreply.github.com>
2022-01-10 20:58:57 +0300

    Add files via upload

---
+++ code.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+from requests import get
+import pandas as pd
+import itertools
+import schedule
+import time
+import random
+from time import sleep
+from datetime import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+headers = ({'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'})
+
+cars_url = 'https://www.autoscout24.de/lst/mercedes-benz/gle-(alle)?desc=0&powertype=kw&search_id=1kq573tji4h&cy=D&ocs_listing=include&sort=price&ustate=N%2CU&atype=C'
+r = get(cars_url, headers=headers)
+page_html = BeautifulSoup(r.text, 'html.parser')
+
+car_containers = page_html.find_all('article', class_="cldt-summary-full-item css-1ypjwms")
+
+firstcar = car_containers[0]
+
+firstcar.find_all('span')
+
+
+print("Name Result " + firstcar.find_all('span')[0].text) 
+firstspan=firstcar.find_all('span')[5].text
+print('First result:' + firstspan) 
+secondspan=firstcar.find_all('span')[6].text
+print('Second result:' + secondspan) 
+thirdspan=firstcar.find_all('span')[7].text
+print('Third result:' + thirdspan)  
+
+
+link_containers = page_html.find_all('div', class_="css-zjik7")
+
+firstlink=link_containers[0]
+
+for a in firstlink.find_all('a', href=True):
+	link_car = a['href']
+	print("Found the URL:", a['href'])
+
+price=firstcar.find_all('span')[5].text
+price = ''.join(filter(lambda i: i.isdigit(), price))
+
+price = int(price)
+
+limit = 65000
+
+if price<limit:
+	if car_containers != []:
+		for container in car_containers:
+			def crawlcar():
+				schedule.every(30).seconds.do(crawcar)
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = '5064855251:AAEDwrCkLxIcjqMR97KHCzezhMw7hPeX3Wo'
+    bot_chatID = '1820102470'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    response = requests.get(send_text)
+    return response.json()
+message = telegram_bot_sendtext('A new car has been found!' +'\n'+ 'Price :' + str('€ '+str(price)) + '\n' + 'KM :' + firstcar.find_all('span')[6].text + '\n' + 'Model :' + firstcar.find_all('span')[7].text + '\n' +
+ 'Link: ' + 'www.autoscout24.de' + link_car)
